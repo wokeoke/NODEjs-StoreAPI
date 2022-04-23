b76cdb859798323d203c3064047eaee6abd30ef1
wokeoke <woke.oke@gmail.com>
2022-04-23 08:52:44 +0700

    Refactor to QueryObject

--- controllers/products.js
+++ controllers/products.js
@@ -4,14 +4,22 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
     // featured: true,
-    name: 'vase table',
+    // name: 'vase table',
+    page: '2',
   });
   res.status(200).json({ nbHits: products.length, products });
 };
 
 // DYNAMIC APPROACH
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
 
   res.status(200).json({ nbHits: products.length, products });
 };
