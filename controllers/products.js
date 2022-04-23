a02d9d356bbc0933ec0379abd149767dc1b56952
wokeoke <woke.oke@gmail.com>
2022-04-23 08:38:48 +0700

    Basic Find

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,11 @@
+const Products = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors');
-  res.status(200).json({ message: 'Products Testing Route' });
+  const products = await Products.find({
+    // featured: true,
+    name: 'vase table',
+  });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
