8be4a795a61d89f4df6241f3d3da0bc6d7b1e325
wokeoke <woke.oke@gmail.com>
2022-04-23 10:00:25 +0700

    Name

--- controllers/products.js
+++ controllers/products.js
@@ -2,25 +2,30 @@ const Product = require('../models/product');
 
 // MANUAL APPROACH
 const getAllProductsStatic = async (req, res) => {
+  const search = 'chair';
   const products = await Product.find({
     // featured: true,
+    // page: '2',
     // name: 'vase table',
-    page: '2',
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ nbHits: products.length, products });
 };
 
 // DYNAMIC APPROACH
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
-
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
 
   if (company) {
     queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
 
   console.log(queryObject);
