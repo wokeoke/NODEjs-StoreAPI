2161758f60d8f1403b302666c98e5c73e0240a43
wokeoke <woke.oke@gmail.com>
2022-04-23 11:07:38 +0700

    Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -51,8 +51,15 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
-  const products = await result;
+  // 23 products / limit 7 products per page
+  //  4 pages / 7 7 7 2
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 7;
+  const skip = (page - 1) * limit;
 
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
