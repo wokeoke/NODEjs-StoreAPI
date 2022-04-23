f0166170700bc44aaced95fd9d2afab2e1b5f84d
wokeoke <woke.oke@gmail.com>
2022-04-23 08:46:54 +0700

    Query Params

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,19 @@
-const Products = require('../models/product');
+const Product = require('../models/product');
 
+// MANUAL APPROACH
 const getAllProductsStatic = async (req, res) => {
-  const products = await Products.find({
+  const products = await Product.find({
     // featured: true,
     name: 'vase table',
   });
   res.status(200).json({ nbHits: products.length, products });
 };
 
+// DYNAMIC APPROACH
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ message: 'Products Route' });
+  const products = await Product.find(req.query);
+
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
