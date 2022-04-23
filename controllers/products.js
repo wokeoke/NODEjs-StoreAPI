428c2c60ba86c224f48331c7f40ff65b38fb7dc5
wokeoke <woke.oke@gmail.com>
2022-04-23 10:16:25 +0700

    Sort - getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -16,7 +16,7 @@ const getAllProductsStatic = async (req, res) => {
 
 // DYNAMIC APPROACH
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -30,8 +30,16 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    // console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
 
   res.status(200).json({ nbHits: products.length, products });
 };
